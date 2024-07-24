fc0fea1b50a9f73ad301e916197ccf4a70db0713
Joaquin Brassinne <146142901+joaquinBrassinne@users.noreply.github.com>
2024-07-24 10:56:30 -0300

    Update ApiS16Application.java

--- src/main/java/com/nocountry/apiS16/ApiS16Application.java
+++ src/main/java/com/nocountry/apiS16/ApiS16Application.java
@@ -14,18 +14,4 @@ public class ApiS16Application {
 		SpringApplication.run(ApiS16Application.class, args);
 	}
 
-	@Configuration
-	public static class Myconfiguration{
-		@Bean
-		public WebMvcConfigurer corsConfigurer(){
-			return new WebMvcConfigurer() {
-				@Override
-				public void addCorsMappings(CorsRegistry registry) {
-					registry.addMapping("/**")
-							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
-				}
-			};
-		}
-	}
-
 }
